1a2f31116a9815758d7ef33d90cb5696ff1f3f2b
can-luca.spiegel <can-luca.spiegel@lekkerland.com>
2025-08-16 19:51:06 +0200

    Use sprite groups for better scalability

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids!")
@@ -21,8 +25,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for sprite in drawable: 
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
